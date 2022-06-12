95a491fd64f5b797d900771592628117d0cca052
minooor <minorilax13@gmail.com>
2022-06-12 02:34:02 +0000

    practice_2

---
+++ practice/step2/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    int r = new java.util.Random().nextInt(90);
+    System.out.println("あなたはたぶん" + r +"歳ですね？");
+
+    int x = 5;
+    int y = 10;
+    String ans = "x+yは" + (x + y);
+    System.out.println(ans);
+
+    int m = 11;
+    System.out.println("m=" + m);
+  }
+}
---
+++ practice/step2/Math.java
@@ -0,0 +1,13 @@
+public class Math {
+  public static void main(String[] args) {
+    int x = 13;
+    int y = 17;
+    System.out.println("x=" + x + ",y=" + y);
+
+    int s = 13 + 17;
+    System.out.println("s=" + s);
+
+    int m = 13 * 17;
+    System.out.println("m=" + m);
+  }
+}
