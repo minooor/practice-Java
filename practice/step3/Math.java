4b3e2835cb87f17c39bff0d5d8121601608bd33f
minooor <minorilax13@gmail.com>
2022-06-16 01:36:13 +0000

    practice_3

---
+++ practice/step3/Math.java
@@ -0,0 +1,11 @@
+public class Math {
+  public static void main(String[] args) {
+    int sum=0;
+    for(int i = 1; i < 100; i ++) {
+      if(i%2 != 0) {
+        sum+=i;
+      }
+    }
+    System.out.println("1から100までの奇数の和は" + sum);
+  }
+}
