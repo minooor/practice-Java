afd092f849de61175d3950dff39af8241dd27f16
minooor <minorilax13@gmail.com>
2022-06-24 06:24:07 +0000

    practice_3-3

---
+++ practice/step3/step3-3/Number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Number {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    if (a >= 10 && b >= 10) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+  }
+}
