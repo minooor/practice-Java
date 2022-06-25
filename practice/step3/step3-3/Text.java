e2ce16aa4f1f1519505a736e94ed1b751b0275ff
minooor <minorilax13@gmail.com>
2022-06-25 12:29:57 +0000

    practice_3-3

---
+++ practice/step3/step3-3/Text.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Text {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char c = s.charAt(0);
+
+        if (Character.isUpperCase(c)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        sc.close();
+    }
+}
